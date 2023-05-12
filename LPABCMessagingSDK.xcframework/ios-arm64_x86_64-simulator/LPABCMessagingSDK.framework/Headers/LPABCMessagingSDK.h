78b3379a3c9ecbe905f901c79df9d776ddb7579e
Grant <ghasfjord@liveperson.com>
2023-05-12 09:45:43 -0700

    Adding new XCFramework for release

---
+++ LPABCMessagingSDK.xcframework/ios-arm64_x86_64-simulator/LPABCMessagingSDK.framework/Headers/LPABCMessagingSDK.h
@@ -0,0 +1,20 @@
+//
+//  LPABCMessagingSDK.h
+//  LPABCMessagingSDK
+//
+//  Created by Grant Hasfjord on 4/17/23.
+//  Copyright © 2023 LivePerson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for LPABCMessagingSDK.
+FOUNDATION_EXPORT double LPABCMessagingSDKVersionNumber;
+
+//! Project version string for LPABCMessagingSDK.
+FOUNDATION_EXPORT const unsigned char LPABCMessagingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LPABCMessagingSDK/PublicHeader.h>
+
+
